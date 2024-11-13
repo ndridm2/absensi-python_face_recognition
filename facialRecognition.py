f665b22b21b7797af4d98c8435597505645d9383
ndridm2 <andri.darmuli26@gmail.com>
2024-11-13 13:06:32 +0700

    fix(update): size cam

--- facialRecognition.py
+++ facialRecognition.py
@@ -1,6 +1,7 @@
 import time
 import cv2
 import cv2.data
+import tkinter as tk
 from database import fetchUser, storeAttendance
 
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -22,8 +23,20 @@ recognizer.read('trainer/trainer.yml')
 faceDetector = cv2.CascadeClassifier(cascade_path)
 
 cam = cv2.VideoCapture(0)
-cam.set(3, 640)
+cam.set(3, 650)
 cam.set(4, 480)
+
+# Mendapatkan ukuran layar untuk menentukan posisi tengah
+root = tk.Tk()
+root.withdraw()  # Menyembunyikan jendela Tkinter
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Menghitung posisi jendela webcam agar berada di tengah layar
+window_width = 650  # Menambahkan padding
+window_height = 480
+x_offset = int((screen_width - window_width) / 2)
+y_offset = 0
 
 while True:
     ret, frame = cam.read()
@@ -41,14 +54,16 @@ while True:
                 user = fetchUser(id)
                 userId = user['id']
                 username = user['name']
-                
-                cv2.putText(frame, str(username), (x+5, y+5), font, 1, (255, 255, 255))
+                text_x = x + 5
+                text_y = y - 10
+
+                cv2.putText(frame, str(username), (text_x, text_y), font, 1, (255, 255, 255))
 
                 if userId not in notMe:
                     recognition = False
                     ask = True
             else:
-                cv2.putText(frame, str('unknown'), (x+5, y+5), font, 1, (255, 255, 255))
+                cv2.putText(frame, str('unknown'), (text_x, text_y), font, 1, (255, 255, 255))
 
     if(saving):
         cv2.putText(frame, username + ' Attendaces', (30, 420), font, 0.8, (0, 255, 0))
@@ -67,12 +82,13 @@ while True:
             saving = False
 
     elif(ask):
-        cv2.putText(frame, "Apakah kamu?", (30, 30), font, 0.6, (255, 255, 255))
-        cv2.putText(frame, username, (30, 60), font, 0.6, (255, 255, 255))
-        cv2.putText(frame, "no (x)", (30, 90), font, 0.5, (0, 0, 255))
-        cv2.putText(frame, "| yes (enter)", (100, 90), font, 0.5, (0, 255, 0))
+        cv2.putText(frame, "apakah kamu?", (30, 30), font, 0.6, (255, 255, 255))
+        cv2.putText(frame, username, (30, 50), font, 0.6, (255, 255, 255))
+        cv2.putText(frame, "no (x)", (30, 70), font, 0.5, (255, 255, 255))
+        cv2.putText(frame, "| yes (enter)", (100, 70), font, 0.5, (255, 255, 255))
 
     cv2.imshow('Face Recognition', frame)
+    cv2.moveWindow('Face Recognition', x_offset, y_offset)  # Mengatur posisi jendela
 
     k = cv2.waitKey(10)
     if(k == 27): # esc
